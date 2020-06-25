bd5dc1d3b70c8f17f99afc9fd153c3b4fffe52b9
mandarhumbare <mandar.humbare@gmail.com>
2020-06-25 16:33:13 +0530

    Add files via upload

---
+++ AuthenticationHelper/BasicAuthAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthenticationHelper
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class BasicAuthAttribute : TypeFilterAttribute
+    {
+        public BasicAuthAttribute(string realm = @"My Realm") : base(typeof(BasicAuthFilter))
+        {
+            Arguments = new object[] { realm };
+        }
+    }
+}
